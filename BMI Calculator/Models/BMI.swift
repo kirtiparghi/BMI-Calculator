70e51b406b98b1eb7859a21b6b9e49cdab9b735c
Kirti Parghi <kirtiparghi@gmail.com>
2021-04-06 12:26:26 -0400

    commit

---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Kirti Parghi on 06/04/2021.
+//  
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
